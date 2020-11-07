358027105fb08f50ab0f4f54b65f5f84f7c86a66
gulukai <1841259689@qq.com>
2020-11-07 21:00:28 +0800

    poetry

--- app/src/main/java/com/example/test/fragment/SentenceFragment.kt
+++ app/src/main/java/com/example/test/fragment/SentenceFragment.kt
@@ -1,7 +1,23 @@
 package com.example.test.fragment
 
+import android.os.Bundle
+import android.view.View
+import androidx.fragment.app.Fragment
 import com.example.test.R
+import com.example.test.adapter.MyViewPagerAdapter
+import com.example.test.sentencefragment.TheAncientsFragment
+import com.example.test.sentencefragment.ThreeWordsFragment
+import kotlinx.android.synthetic.main.zanding_fragment_layout.*
 
 class SentenceFragment : BaseFragment(R.layout.zanding_fragment_layout) {
-
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val fragmentList = listOf<Fragment>(ThreeWordsFragment(), TheAncientsFragment())
+        val tabList = arrayListOf("五言佳句", "知古人")
+        ViewPager_sentence_fragment.adapter = MyViewPagerAdapter(childFragmentManager, fragmentList)
+        TabLayout_sentence_fragment.setupWithViewPager(ViewPager_sentence_fragment)
+        for (i in 0 until tabList.size) {
+            TabLayout_sentence_fragment.getTabAt(i)?.text = tabList[i]
+        }
+    }
 }
---
+++ app/src/main/java/com/example/test/sentencefragment/TheAncientsFragment.kt
@@ -0,0 +1,13 @@
+package com.example.test.sentencefragment
+
+import android.os.Bundle
+import android.view.View
+import com.example.test.R
+import com.example.test.fragment.BaseFragment
+
+class TheAncientsFragment : BaseFragment(R.layout.the_ancients_fragment_layout){
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+    }
+}
